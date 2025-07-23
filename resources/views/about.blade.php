49b43d98eaf4d1b29aef1f134ebf050327bcc006
andrijanikic23 <andrijanikic001@gmail.com>
2025-07-23 10:42:08 +0200

    * About page

---
+++ resources/views/about.blade.php
@@ -0,0 +1,9 @@
+@extends("layout")
+
+@section("content")
+    <article class="container" style="margin-top:80px;">
+        <h1>Welcome to our store!</h1>
+        <p>We specialize in high-quality iPhone cases designed to protect your device while adding style and personality. Our mission is to combine durability with sleek design, offering you reliable protection without compromising on looks. With attention to detail and premium materials, we ensure your iPhone stays safe and stands out. Thank you for choosing us to safeguard your device!On our site, you can also find a range of additional iPhone accessories, such as charger adapters, headphones, and more.</p>
+    </article>
+
+@endsection
