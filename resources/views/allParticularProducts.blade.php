21d537d1cf15b65b0dcd9dd70636d844f5d27992
andrijanikic23 <andrijanikic001@gmail.com>
2025-07-29 14:01:14 +0200

    * Universal for any type of product

--- resources/views/allParticularProducts.blade.php
+++ resources/views/allParticularProducts.blade.php
@@ -27,7 +27,7 @@
                             <h5 class="card-title">{{ $product["name"] }}</h5>
                             <p class="card-text">{{ $product["description"] }}</p>
                             <p class="card-text">{{ $product["price"] }}RSD</p>
-                            <a href="#" class="btn btn-primary">Add to cart</a>
+                            <a href="{{ route('product.view', ["product" => $product["id"]]) }}" class="btn btn-primary">View product</a>
                         </div>
                     </div>
                 @endforeach
