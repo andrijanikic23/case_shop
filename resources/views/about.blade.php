1832be711cb655a5d9823b66d20a67ec913d82ce
andrijanikic23 <andrijanikic001@gmail.com>
2025-07-29 13:59:43 +0200

    * New about part

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,9 +1,13 @@
 @extends("layout")
 
 @section("content")
-    <article class="container" style="margin-top:80px;">
-        <h1>Welcome to our store!</h1>
-        <p>We specialize in high-quality iPhone cases designed to protect your device while adding style and personality. Our mission is to combine durability with sleek design, offering you reliable protection without compromising on looks. With attention to detail and premium materials, we ensure your iPhone stays safe and stands out. Thank you for choosing us to safeguard your device!On our site, you can also find a range of additional iPhone accessories, such as charger adapters, headphones, and more.</p>
+    <article class="container d-flex col-8" style="margin-top:80px;">
+        <div class="p-3">
+            <h1>Welcome to MaskeShop.rs!</h1>
+            <p>We specialize in high-quality iPhone cases designed to protect your device while adding style and personality. Our mission is to combine durability with sleek design, offering you reliable protection without compromising on looks. With attention to detail and premium materials, we ensure your iPhone stays safe and stands out. Thank you for choosing us to safeguard your device!On our site, you can also find a range of additional iPhone accessories, such as charger adapters, headphones, and more.</p>
+        </div>
+
+        <img class="col-4" src="https://www.shutterstock.com/image-photo/happy-mid-aged-older-business-600nw-2322385015.jpg">
     </article>
 
 @endsection
