11800b756ce6ce18d215d2bf1a4f743f579d2f46
andrijanikic23 <andrijanikic001@gmail.com>
2025-07-29 14:00:09 +0200

    * Navigation corrections

--- resources/views/navigation.blade.php
+++ resources/views/navigation.blade.php
@@ -42,7 +42,7 @@
                     </a>
                 </li>
                 <li class="nav-item ps-2">
-                    <a href="/cart" class="nav-link">
+                    <a href="{{ route('profile.edit') }}" class="nav-link">
                         <i class="fa-solid fa-user"></i>
                     </a>
                 </li>
