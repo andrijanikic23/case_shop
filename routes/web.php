b25a9caa44ad6779b55e0216b08cea4a01543fe8
andrijanikic23 <andrijanikic001@gmail.com>
2025-08-07 13:46:59 +0200

    * Admin middleware and admin routes

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\OrdersController;
 use App\Http\Controllers\ProductsController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ReviewsController;
+use App\Http\Middleware\AdminMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/dashboard', function () {
@@ -23,7 +24,6 @@ Route::view("/about", "about")->name("about");
 Route::view("/", "welcome")->name("welcome");
 
 
-
 Route::controller(ProductsController::class)->prefix("product")->group(function(){
     Route::post("/added","add")->name("product.added");
     Route::get("/cases/all/{categoryId}","products")->name("product.cases");
@@ -33,12 +33,11 @@ Route::controller(ProductsController::class)->prefix("product")->group(function(
     Route::get("/view/{product}","view")->name("product.view");
 });
 
-Route::view("/product/add", "productAdd")->name("product.add");
-
+Route::view("/product/add", "productAdd")->middleware("auth", AdminMiddleware::class)->name("product.add");
 
 Route::post("/review/rating", [ReviewsController::class, "rate"])->name("review.stars");
 
-Route::controller(CartController::class)->prefix("cart")->group(function(){
+Route::controller(CartController::class)->prefix("cart")->middleware("auth")->group(function(){
     Route::post("/added", "addProduct")->name("cart.add");
     Route::get("/overview", "display")->name("cart.display");
     Route::post("/overview/minus-product", "minusCartItem")->name("cart.minus");
@@ -48,6 +47,10 @@ Route::controller(CartController::class)->prefix("cart")->group(function(){
 Route::post("/order/finished", [OrdersController::class, "finished"])->name("order.finished");
 
 Route::view("/contact", "contact")->name("contact.info");
+
+Route::view("/job/openings", "jobOpenings")->name("job.openings");
+
+
 
 
 
