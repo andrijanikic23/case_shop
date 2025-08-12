a1fc22acffa3520947bad4c4bc8dd08ef5e72798
andrijanikic23 <andrijanikic001@gmail.com>
2025-08-12 13:04:38 +0200

    * Cart layout correction

--- resources/views/cartLayout.blade.php
+++ resources/views/cartLayout.blade.php
@@ -9,6 +9,8 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+
+    @yield("head")
 </head>
 <body>
 
