0fee140d41421c1d3f527eb3c2f75524f59e18d3
andrijanikic23 <andrijanikic001@gmail.com>
2026-01-03 18:31:50 +0100

    * Footer position

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,3 +1,3 @@
-<footer style="background-color: #222; color: #fff; text-align: center; padding: 10px;">
+<footer class="mt-4 w-100" style="background-color: #222; color: #fff; text-align: center; padding: 10px; bottom:0;">
     &copy; 2025 MaskeShop.rs. All rights reserved.
 </footer>
