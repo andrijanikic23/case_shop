a1d4b139607af2fb335e5ce400e94bfbadb00d5f
andrijanikic23 <andrijanikic001@gmail.com>
2026-01-05 20:56:36 +0100

    * About page and footer modification

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,13 +3,16 @@
 @section("title", "About")
 
 @section("content")
-    <article class="container d-flex col-8" style="margin-top:80px;">
+    <article class="container d-flex flex-column col-8" style="margin-top:80px;">
         <div class="p-3">
             <h1>Welcome to MaskeShop.rs!</h1>
             <p>We specialize in high-quality iPhone cases designed to protect your device while adding style and personality. Our mission is to combine durability with sleek design, offering you reliable protection without compromising on looks. With attention to detail and premium materials, we ensure your iPhone stays safe and stands out. Thank you for choosing us to safeguard your device!On our site, you can also find a range of additional iPhone accessories, such as charger adapters, headphones, and more.</p>
         </div>
 
-        <img class="col-4" src="https://www.shutterstock.com/image-photo/happy-mid-aged-older-business-600nw-2322385015.jpg">
+        <div class="text-center">
+            <img class="col-8 vh-80" src="{{ asset('/images/logo.png') }}">
+        </div>
+
     </article>
 
 @endsection
--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,3 +1,3 @@
-<footer class="mt-4 w-100" style="background-color: #222; color: #fff; text-align: center; padding: 10px; bottom:0;">
+<footer class="mt-4 w-100" style="background-color: #222; color: #fff; text-align: center; padding: 10px;">
     &copy; 2025 MaskeShop.rs. All rights reserved.
 </footer>
